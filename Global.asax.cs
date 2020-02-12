ad999bfbaf4a6a151e0d0fc4f78468a577162d2e
Bell Yard London <60955933+bellyardlondon@users.noreply.github.com>
2020-02-12 09:35:13 +0400

    Inital setup commit

---
+++ Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace BellYard.Web
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+           AreaRegistration.RegisterAllAreas();
+           RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(object));
+            log.Fatal("Unhandled Exception", Server.GetLastError());
+
+            Server.ClearError();
+        }
+    }
+}
